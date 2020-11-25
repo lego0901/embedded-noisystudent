b9431117f067d2ed63d5003751998a742d67ee2d
Woosung Song <lego0901@gmail.com>
2020-11-26 00:44:49 +0900

    Add complete ResNet model
    - stochastic depth
    - dropout for the final fc

--- model.py
+++ model.py
@@ -35,87 +35,80 @@ class Downsample(nn.Module):
 
 
 class StoDepth_BasicBlock(nn.Module):
-    def __init__(self, prob, inplanes, planes, norm_layer, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, prob, norm_layer, stride=1, downsample=None):
         super(StoDepth_BasicBlock, self).__init__()
         self.downsample = downsample
         self.stride = stride
         self.norm_layer = norm_layer
 
-        self.bn1 = norm_layer(inplanes)
-        self.relu1 = nn.ReLU()
         self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu1 = nn.ReLU()
 
+        self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.relu2 = nn.ReLU()
-        self.conv2 = conv3x3(planes, planes)
 
         self.prob = prob
         self.m = torch.distributions.bernoulli.Bernoulli(torch.Tensor([self.prob]))
 
     def forward(self, x):
         residual = x
+        batch_size = x.shape[0]
 
         if self.training:
-            if torch.equal(self.m.sample(), torch.ones(1)):
-                self.conv1.weight.requires_grad = True
-                self.conv2.weight.requires_grad = True
+            sample = self.m.sample(sample_shape=torch.Size([batch_size, 1, 1])).to(x.device)
+            sample.requires_grad = False
+            residual = residual.mul(sample)
 
-                residual = self.bn1(residual)
-                residual = self.relu1(residual)
-                residual = self.conv1(residual)
-
-                residual = self.bn2(residual)
-                residual = self.relu2(residual)
-                residual = self.conv2(residual)
-
-                if self.downsample is not None:
-                    x = self.downsample(x)
-                residual += x
-            else:
-                self.conv1.weight.requires_grad = False
-                self.conv2.weight.requires_grad = False
-
-                if self.downsample is not None:
-                    x = self.downsample(x)
-                residual = x
-        else:
+            residual = self.conv1(residual)
             residual = self.bn1(residual)
             residual = self.relu1(residual)
-            residual = self.conv1(residual)
 
-            residual = self.bn2(residual)
-            residual = self.relu2(residual)
             residual = self.conv2(residual)
+            residual = self.bn2(residual)
 
             if self.downsample is not None:
                 x = self.downsample(x)
-            residual += self.prob * x
+            residual = residual + x
+        else:
+            residual = self.conv1(residual)
+            residual = self.bn1(residual)
+            residual = self.relu1(residual)
 
+            residual = self.conv2(residual)
+            residual = self.bn2(residual)
+
+            if self.downsample is not None:
+                x = self.downsample(x)
+            residual = residual * self.prob + x
+
+        residual = self.relu2(residual)
         return residual
 
 
 class StoDepth_ResNet(nn.Module):
-    def __init__(self, prob_0_L, num_layers, num_classes=10, norm_layer=nn.BatchNorm2d):
+    def __init__(
+        self,
+        num_layers,
+        prob_0_L=(1.0, 0.8),
+        dropout_prob=0.5,
+        num_classes=10,
+        norm_layer=nn.BatchNorm2d,
+    ):
         super(StoDepth_ResNet, self).__init__()
-
-        self.prob = prob_0_L[0]
-        self.prob_step = (prob_0_L[0] - prob_0_L[1]) / (num_layers - 1)
 
         self.num_layers = num_layers
         assert (num_layers - 2) % 6 == 0
         self.N = (self.num_layers - 2) // 6
 
+        self.prob = prob_0_L[0]
+        self.prob_step = (prob_0_L[0] - prob_0_L[1]) / (self.N * 3)
+
         self.num_classes = num_classes
         self.norm_layer = norm_layer
 
-        self.conv = nn.Conv2d(
-            in_channels=3,
-            out_channels=16,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False,
-        )
+        self.conv = nn.Conv2d(3, 16, 3, padding=1, bias=False)
         self.inplanes = 16
         self.layer1 = self._make_layers(planes=16, stride=1)
         self.layer2 = self._make_layers(planes=32, stride=2)
@@ -123,15 +116,14 @@ class StoDepth_ResNet(nn.Module):
         self.bn = norm_layer(64)
         self.relu = nn.ReLU()
         self.avgpool = nn.AvgPool2d(8)
+        self.drop = nn.Dropout(p=dropout_prob)
         self.fc = nn.Linear(64, num_classes)
 
         # weight initialization
         for module in self.modules():
             if isinstance(module, nn.Conv2d):
                 n = module.kernel_size[0] * module.kernel_size[1] * module.out_channels
-                module.weight.data.normal_(
-                    0, torch.sqrt(torch.Tensor([2.0 / n])).item()
-                )
+                module.weight.data.normal_(0, torch.sqrt(torch.Tensor([2.0 / n])).item())
 
     def _make_layers(self, planes, stride):
         if stride == 2:
@@ -142,9 +134,9 @@ class StoDepth_ResNet(nn.Module):
         layers_list = nn.ModuleList(
             [
                 StoDepth_BasicBlock(
-                    self.prob,
                     self.inplanes,
                     planes,
+                    self.prob,
                     self.norm_layer,
                     stride,
                     down_sample,
@@ -155,9 +147,7 @@ class StoDepth_ResNet(nn.Module):
         self.prob -= self.prob_step
 
         for _ in range(self.N - 1):
-            layers_list.append(
-                StoDepth_BasicBlock(self.prob, planes, planes, self.norm_layer)
-            )
+            layers_list.append(StoDepth_BasicBlock(planes, planes, self.prob, self.norm_layer))
             self.prob -= self.prob_step
 
         return nn.Sequential(*layers_list)
@@ -171,6 +161,6 @@ class StoDepth_ResNet(nn.Module):
         x = self.relu(x)
         x = self.avgpool(x)
         x = x.flatten(start_dim=1)
+        x = self.drop(x)
         x = self.fc(x)
-
         return x
